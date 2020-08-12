483eb91aa6368b31f4bfd3585aa480011c2a0647
ccakir <59564041+ccakir@users.noreply.github.com>
2020-08-12 19:13:16 +0200

    Update RestPreconditions.java

--- src/main/java/com/cakir/util/RestPreconditions.java
+++ src/main/java/com/cakir/util/RestPreconditions.java
@@ -9,14 +9,7 @@ public final class RestPreconditions{
 		throw new AssertionError();
 	}
 	
-	// API
 	
-	/**
-	 * Ensures that an object reference passed as a parameter to the calling method is not null.
-	 * @param reference an object reference
-	 * @return the non-null reference that was validated
-	 * @throws ResourceNotFoundException if {@code reference} is null
-	 */
 	public static < T >T checkNotNull( final T reference ){
 		if( reference == null ){
 			throw new ResourceNotFoundException();
@@ -24,12 +17,7 @@ public final class RestPreconditions{
 		return reference;
 	}
 	
-	/**
-	 * Ensures that an object reference passed as a parameter to the calling method is not null.
-	 * @param reference an object reference
-	 * @return the non-null reference that was validated
-	 * @throws BadRequestException if {@code reference} is null
-	 */
+	
 	public static < T >T checkRequestElementNotNull( final T reference ){
 		if( reference == null ){
 			throw new BadRequestException();
